77b6748e6beff7359d5d549574edaf38bf4d85ef
Laura Trentino <laura@trentino.com.au>
2021-12-17 16:19:19 +0800

    added filter search, but having troubles with table cols disappearing?

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,3 +18,34 @@ tableData.forEach((data_row) => {
     table_row.append("td").text(data_row.durationMinutes);
     table_row.append("td").text(data_row.comments);
 });
+
+
+// filter button
+var button = d3.select("#filter-btn")
+
+// 
+button.on("click", function(event) {
+    d3.event.preventDefault();
+    tbody.html("");
+    
+    
+    var input_element = d3.select("#datetime");
+    var input_value = input_element.property("value");
+
+
+    var tbody_filtered = d3.select("#ufo-table"); //this was to try keep the table cols names but not working.
+
+var filteredData = tableData.filter(tableData => tableData.datetime === input_value);
+filteredData.forEach((data_row) => {
+    var table_row = tbody_filtered.append("tr");  // not sure why not appending the cols names?
+    table_row.append("td").text(data_row.datetime);
+    table_row.append("td").text(data_row.city);
+    table_row.append("td").text(data_row.state);
+    table_row.append("td").text(data_row.country);
+    table_row.append("td").text(data_row.shape);
+    table_row.append("td").text(data_row.durationMinutes);
+    table_row.append("td").text(data_row.comments);
+
+});
+});
+
