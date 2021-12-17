e1cf2cf81d30d0b393d09e45d284bf2e48372356
Laura Trentino <laura@trentino.com.au>
2021-12-17 15:30:20 +0800

    appended table rows

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// checking data
+console.log(tableData);
+
+// get reference to table body
+var tbody = d3.select("#ufo-table");
+
+// append table rows
+tableData.forEach((data_row) => {
+    var table_row = tbody.append("tr");
+    table_row.append("td").text(data_row.datetime);
+    table_row.append("td").text(data_row.city);
+    table_row.append("td").text(data_row.state);
+    table_row.append("td").text(data_row.country);
+    table_row.append("td").text(data_row.shape);
+    table_row.append("td").text(data_row.durationMinutes);
+    table_row.append("td").text(data_row.comments);
+});
